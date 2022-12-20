e93d3e928155b78c6e04ac6c3f74f58cc7123ed0
Sylvester Hesp <github@oisyn.nl>
2022-12-20 14:23:03 +0100

    Optimizations

--- Problem16/Problem16.cpp
+++ Problem16/Problem16.cpp
@@ -14,7 +14,7 @@ int FindChar(const char* ptr, char c)
 	return -1;
 }
 
-constexpr bool Verbose = false;
+constexpr bool Verbose = true;
 
 template<class... T>
 void vprintln(std::_Fmt_string<T...> fmt, T&&... args)
@@ -122,7 +122,7 @@ bool Run(const wchar_t* file)
 				queue.pop_front();
 
 				int c = costs[v] + 1;
-				if ((!valveIdx || valves[valveIdx].rate) && valves[v].rate)
+				if (valveIdx != v && (!valveIdx || valves[valveIdx].rate) && valves[v].rate)
 					valves[valveIdx].valveCosts.emplace_back(v, c);
 				for (auto t : valves[v].tunnels)
 				{
@@ -140,19 +140,34 @@ bool Run(const wchar_t* file)
 			relevantValves.pop_back();
 
 		vprintln("Running part 1");
-		struct State { int rate, time, valveIdx; ullong usedValves; int pureRate; };
+		struct State { int rate, time, valveIdx; ullong usedValves; int pureRate; size_t prevState;  };
 
 		constexpr int MaxTime = 30;
 		std::vector<State> stateQueue;
+		std::vector<std::unordered_map<ullong, size_t>> bestRouteToValve(relevantValves.size());
 		stateQueue.emplace_back(0, MaxTime);
 		if (valves[0].rate)	// if we can open the start valve, also try that as first action
-			stateQueue.emplace_back((MaxTime - 1) * valves[0].rate, MaxTime - 1, 0, 1);
+			stateQueue.emplace_back((MaxTime - 1) * valves[0].rate, MaxTime - 1, 0, 1ull << valves[0].relIdx/*, valves[0].rate*/);
 
-		int statePos = 0;
-		while (statePos < (int)stateQueue.size())
+		auto GetName = [](int idx) { std::string r(2, ' '); r[0] = idx / 26 + 'A'; r[1] = idx % 26 + 'A'; return r; };
+		auto GetPath = [&](int last, size_t pos)
+		{
+			std::string r;
+			r += GetName(last);
+			while (pos)
+			{
+				r.insert(0, GetName(stateQueue[pos].valveIdx) + " -> ");
+				pos = stateQueue[pos].prevState;
+			}
+			return r;
+		};
+
+		size_t statePos = 0;
+		size_t winPos = 0;
+		int endValve = 0;
+		while (statePos < stateQueue.size())
 		{
 			auto state = stateQueue[statePos++];
-
 			auto& valve = valves[state.valveIdx];
 
 			for (auto [v, c] : valve.valveCosts)
@@ -167,12 +182,33 @@ bool Run(const wchar_t* file)
 					continue;
 				newState.rate += newState.time * valves[v].rate;
 				newState.pureRate += valves[v].rate;
-				maxRate = std::max(maxRate, newState.rate);
+				if (maxRate < newState.rate)
+				{
+					maxRate = newState.rate;
+					winPos = statePos - 1;
+					endValve = v;
+				}
 				newState.valveIdx = v;
 				newState.usedValves |= valveMask;
+				newState.prevState = statePos - 1;
+				int potential = (totalFlowRate - newState.pureRate) * (newState.time - 1);
 
-				if (newState.time > 1 && newState.rate + (totalFlowRate - newState.pureRate) * (newState.time - 1) > maxRate)
-					stateQueue.push_back(newState);
+				if (newState.time > 1 && newState.rate + potential > maxRate)
+				{
+					int vRelIdx = valves[v].relIdx;
+					if (auto it = bestRouteToValve[vRelIdx].find(newState.usedValves); it != bestRouteToValve[vRelIdx].end())
+					{
+						auto& s = stateQueue[it->second];
+						int pot2 = (totalFlowRate - s.pureRate) * (s.time - 1);
+						if (newState.rate + potential > s.rate + pot2)
+							s = newState;
+					}
+					else
+					{
+						bestRouteToValve[vRelIdx][newState.usedValves] = stateQueue.size();
+						stateQueue.push_back(newState);
+					}
+				}
 
 				if (newState.time >= 4)
 				{
@@ -183,6 +219,9 @@ bool Run(const wchar_t* file)
 				}
 			}
 		}
+
+		//println("Found {}", GetPath(endValve, winPos));
+
 	} 
 	talgo1.Stop();
 
@@ -190,37 +229,52 @@ bool Run(const wchar_t* file)
 	vprintln("Running part 2");
 	int maxRate2 = 0;
 	{
-		std::vector<std::vector<std::pair<ullong, int>>> scoresByLowedUsedBit(relevantValves.size());
+		const int numValves = relevantValves.size();
+		auto GetIndex = [=](ullong l) { return std::countr_zero(l) * numValves + std::countr_zero(l & (l - 1)); };
+		std::vector<std::vector<std::pair<ullong, int>>> scoresByLowedUsedBits(numValves * numValves);
 
-		vprintln("Grouping results");
+		vprintln("Grouping {} results", maxScores.size());
 		for (auto [m, s] : maxScores)
-		{
-			auto idx = std::countr_zero(m);
-			scoresByLowedUsedBit[idx].emplace_back(m, s);
-		}
+			if (std::popcount(m) > 1)
+				scoresByLowedUsedBits[GetIndex(m)].emplace_back(m, s);
 
-		for (auto& v : scoresByLowedUsedBit)
+		for (auto& v : scoresByLowedUsedBits)
 			std::ranges::sort(v, [](auto& a, auto& b) { return a.second > b.second; });
 
+		vprintln("Sorting paths");
+		std::vector<std::pair<ullong, int>> maxScoresSorted{ maxScores.begin(), maxScores.end() };
+		std::ranges::sort(maxScoresSorted, [](auto& a, auto& b) { return a.second > b.second; });
+		int maxPossibleScore = maxScoresSorted[0].second;
+
 		vprintln("Finding pairs");
-		ullong allValvesMask = (1ull << relevantValves.size()) - 1;
-		for (auto [m, s] : maxScores)
+		ullong allValvesMask = (1ull << numValves) - 1;
+		for (auto [m, s] : maxScoresSorted)
 		{
+			if (s + s < maxRate2)
+				break;
 			ullong inverted = ~m & allValvesMask;
 			while (inverted)
 			{
-				auto idx = std::countr_zero(inverted);
-				for (auto [m2, s2] : scoresByLowedUsedBit[idx])
-				{
-					if (s + s2 < maxRate2)
-						break;
-					if (m & m2)
-						continue;
-					maxRate2 = s + s2;
-					break;
-				}
+				int sidx = std::countr_zero(inverted) * numValves;
 				inverted &= inverted - 1;
+				ullong inv2 = inverted;
+				while (inv2)
+				{
+					int idx = sidx + std::countr_zero(inv2);
+					for (auto [m2, s2] : scoresByLowedUsedBits[idx])
+					{
+						if (s + s2 <= maxRate2)
+							goto nextmask;
+						if (m & m2)
+							continue;
+						maxRate2 = s + s2;
+						//println("{:b}-{:b}: {}", m, m2, maxRate2 - 4*628);
+						break;
+					}
+					inv2 &= inv2 - 1;
+				}
 			}
+		nextmask:;
 		}
 	}
 
@@ -238,14 +292,14 @@ int main()
 	const wchar_t* inputs[] =
 	{
 		//L"example.txt",
-		L"input.txt",
-		L"aoc_2022_day16_large-1.txt",
-		L"aoc_2022_day16_large-2.txt",
-		L"aoc_2022_day16_large-3.txt",
-		L"aoc_2022_day16_large-4.txt",
+		//L"input.txt",
+		//L"aoc_2022_day16_large-1.txt",
+		//L"aoc_2022_day16_large-2.txt",
+		//L"aoc_2022_day16_large-3.txt",
+		//L"aoc_2022_day16_large-4.txt",
 		//L"aoc_2022_day16_large-5.txt",
-		//L"aoc_2022_day16_large-6.txt",
-		L"aoc_2022_day16_large-7.txt",
+		L"aoc_2022_day16_large-6.txt",
+		//L"aoc_2022_day16_large-7.txt",
 	};
 
 	constexpr int NumRuns = 1;
